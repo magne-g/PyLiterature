3730870e30f73be9597ea94f6aa5b6e752e70f14
Magne Granly <magne.granly@hiof.no>
2019-02-10 00:42:02 +0100

    code cleaning

--- modelling/decision_tree_regression.py
+++ modelling/decision_tree_regression.py
@@ -16,6 +16,7 @@ import seaborn as sns
 from sklearn import svm
 from sklearn import metrics
 from datetime import date
+from sklearn.model_selection import validation_curve, learning_curve
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -24,7 +25,13 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 _print_unprocessed_dataset_stats = False
 _print_processed_dataset_stats = False
+_tune_hyper_parameters = True
 _verbose = 1
+
+_important_data = '{}'
+
+
+
 
 
 pd.set_option('display.max_rows', 500)
@@ -34,7 +41,7 @@ pd.set_option('display.max_colwidth', -1)
 pd.set_option('display.max_info_columns', 500)
 
 dot_data = StringIO()
-rng = np.random.RandomState(2)
+rng = np.random.RandomState(42)
 
 
 
@@ -73,13 +80,24 @@ def label_encode(data):
     return data
 
 
-#TODO Move preprocessing
+def save_accuracy_log(scores, estimator):
+
+    filename = np.round(scores.mean(), 4)
+
+    data = pd.Series(estimator).to_frame()
+
+    #params = pd.DataFrame.from_items(data)
+
+    data.to_csv('../logs/' + str(filename))
+
+
+
+
 def preprocess(data):
 
     if _print_unprocessed_dataset_stats:
         print('Unprocessed Dataset Statistics:')
         print_dataset_stats(data)
-
 
     data = data.set_index('finn_code')
     data['price'] = data['price'].astype(np.float)
@@ -147,14 +165,39 @@ def split_data(data, label, test_size=0.2):
                                                         random_state=rng)
     return X_train, X_test, Y_train, Y_test
 
-def define_estimator():
-    estimator = AdaBoostRegressor(RandomForestRegressor(random_state=rng,
-                                                        n_estimators=30,
-                                                        n_jobs=8),
-                                  random_state=rng,
-                                  n_estimators=5)
+def define_estimator(_base_estimator, _meta_estimator, _tune_hyper_parameters):
 
-    return estimator
+    _estimator = _base_estimator
+
+    print(_estimator)
+    if not _tune_hyper_parameters:
+        if(_meta_estimator):
+            return _meta_estimator(_estimator)
+        else:
+            return(_estimator)
+
+    else:
+        parameters = [{'n_estimators': [300],
+                       'max_depth': [15],
+                       'max_features': ['auto','sqrt','log2'],
+                       'min_samples_split': [2],
+                       'min_samples_leaf': [1]
+                       }]
+        print('Starting GridSearchCV with params: ', parameters)
+
+        _cv = GridSearchCV(_base_estimator, param_grid=parameters, n_jobs=8, verbose=2, cv=3, scoring='neg_median_absolute_error', refit=True)
+
+        _cv.fit(X_train, Y_train)
+
+        print(_cv.cv_results_)
+        print(_cv.best_params_)
+        print(_cv.best_score_)
+
+    _estimator = _estimator.set_params(**_cv.best_params_)
+
+    return _estimator
+
+
 
 
 def fit_model(X_train, Y_train, estimator):
@@ -179,11 +222,12 @@ data, label = process_and_label(data, dependant_variable=data['price'])
 
 if(len(data) != len(label)):
     print(len(data), len(label))
-    assert(len(data) == len(label), 'Error: Length not equal in X, Y')
+    assert len(data) == len(label), 'Error: Length not equal in X, Y'
 
 X_train, X_test, Y_train, Y_test = split_data(data, label)
 
-estimator = define_estimator()
+estimator = define_estimator(_base_estimator=RandomForestRegressor(random_state=rng),
+                             _meta_estimator=AdaBoostRegressor(n_estimators=5), _tune_hyper_parameters=True)
 
 model = fit_model(X_train, Y_train, estimator=estimator)
 
@@ -193,8 +237,11 @@ predictions = model.predict(X_test)
 
 result(scores, model, best_param_score, label, predictions)
 
+save_accuracy_log(scores, estimator)
+
 plt.show()
 
+print(model.get_params())
 
 
 
